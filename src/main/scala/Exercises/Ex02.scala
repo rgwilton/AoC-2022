bf4762d10942ba46cb40cdc702bb8159eba7e597
Robert Wilton <robert.public@wilton.org.uk>
2022-12-03 07:39:39 +0000

    Further cleanup of Ex02

--- src/main/scala/Exercises/Ex02.scala
+++ src/main/scala/Exercises/Ex02.scala
@@ -1,8 +1,5 @@
 package aoc
 
-import scala.collection.mutable.Buffer
-import scala.annotation.tailrec
-
 object Ex02 extends Exercise:
  type ParsedInput = Iterator[(Int, Int)]
  override type Common = Array[(Int, Int)]
@@ -11,23 +8,20 @@ object Ex02 extends Exercise:
     for line <- input yield
       (1 + line(0) - 'A', 1 + line(2) - 'X')
     
-
-
   def common(input: ParsedInput) = input.toArray
 
   def part1(common: Common) = 
-    common.collect {
+    common.map {
       // a and b represent rock, papper, or scissors, counting from 1.
       case (a, b) if b == a + 1 || (b == 1 && a == 3) => b + 6 // win
       case (a, b) if a == b => b + 3 // draw
-      case (a, b) => b // lose
+      case (a, b) => b + 0 // lose
     }.sum
 
   def part2(common: Common) =
     // a represents rock, paper, scissors.  b represents lose, draw, win, counting from 1.
-    common.collect {
+    common.map {
       case (a, 3) => 6 + (if a == 3 then 1 else a + 1) // win
-      case (a, 2) => a + 3 // draw
-      case (a, 1) => if (a == 1) then 3 else a - 1 // lose
+      case (a, 2) => 3 + a // draw
+      case (a, 1) => 0 + (if a == 1 then 3 else a - 1) // lose
     }.sum
-
