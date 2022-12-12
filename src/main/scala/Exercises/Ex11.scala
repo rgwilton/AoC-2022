f9a69d1b3c6c05af1417e5c98bc4d039e0123773
Robert Wilton <robert.public@wilton.org.uk>
2022-12-12 13:06:42 +0000

    Fix day 11 bug

--- src/main/scala/Exercises/Ex11.scala
+++ src/main/scala/Exercises/Ex11.scala
@@ -7,8 +7,7 @@ object Ex11 extends Exercise:
   type ParsedInput = Seq[Monkey]
   override type Common = Seq[Monkey]
   case class Move(monkeyId: Int, worry: Long)
-  var modulus = 1L
-  class Monkey(val id: Int, val initItems: Seq[Int], val throwItem: (Long, Long, Long) => Move):
+  class Monkey(val id: Int, val initItems: Seq[Int], val divisor: Int, val throwItem: (Long, Long, Long) => Move):
     val itemWorries: mutable.Queue[Long] = mutable.Queue(initItems.map(_.toLong): _*)
     var inspected: Int = 0
     override def toString = s"Monkey $id: ${itemWorries.toSeq.mkString(",")}"
@@ -24,7 +23,6 @@ object Ex11 extends Exercise:
 
     def monkey[p: P]: P[Monkey] = P(intro ~/ startingItems ~/ operation ~/ divisble ~/ ifTrue ~/ ifFalse).map {
       (id, items, op, divisor, ifTrue, ifFalse) => 
-        modulus *= divisor
         val fn = (old: Long, boredem: Long, modulus: Long) =>
           val worryLevel = 
             ((op match
@@ -35,7 +33,7 @@ object Ex11 extends Exercise:
           val nextMonkey = if worryLevel % divisor == 0 then ifTrue else ifFalse
           Move(nextMonkey, worryLevel)
 
-        Monkey(id.toInt, items, fn)
+        Monkey(id.toInt, items, divisor, fn)
     }
     def monkeys[p: P]: P[Seq[Monkey]] = P(monkey.rep(sep = "") ~ End)
 
@@ -47,23 +45,27 @@ object Ex11 extends Exercise:
   def common(input: ParsedInput) = input
 
   def part1(monkeySeq: Seq[Monkey]) =
-    val monkeys = monkeySeq.map(m => Monkey(m.id, m.initItems, m.throwItem))
+    val monkeys = monkeySeq.map(m => Monkey(m.id, m.initItems, m.divisor, m.throwItem))
+    var modulus = (for m <- monkeys yield m.divisor.toLong).product
+
     for round <- 1 to 20 do
       for monkey <- monkeys do
         while monkey.itemWorries.nonEmpty do
           monkey.inspected += 1
-          val Move(id, worry) = monkey.throwItem(monkey.itemWorries.dequeue(), 3, modulus)
+          val Move(id, worry) = monkey.throwItem(monkey.itemWorries.dequeue(), 3L, modulus)
           monkeys(id).itemWorries += (worry)
       
     monkeys.map(_.inspected).sorted.takeRight(2).map(_.toLong).product
 
   def part2(monkeySeq: Seq[Monkey]) =
-    val monkeys = monkeySeq.map(m => Monkey(m.id, m.initItems, m.throwItem))
+    val monkeys = monkeySeq.map(m => Monkey(m.id, m.initItems, m.divisor, m.throwItem))
+    var modulus = (for m <- monkeys yield m.divisor.toLong).product
+
     for round <- 1 to 10_000 do
       for monkey <- monkeys do
         while monkey.itemWorries.nonEmpty do
           monkey.inspected += 1
-          val Move(id, worry) = monkey.throwItem(monkey.itemWorries.dequeue(), 1, modulus)
+          val Move(id, worry) = monkey.throwItem(monkey.itemWorries.dequeue(), 1L, modulus)
           monkeys(id).itemWorries += (worry)
 
     monkeys.map(_.inspected).sorted.takeRight(2).map(_.toLong).product
